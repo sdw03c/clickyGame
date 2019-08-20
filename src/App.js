94abe2c34973090db4b892ef46807195de4aa083
sdw03c <sharn.dwong@gmail.com>
2019-08-20 15:57:13 -0400

    App.js logic

--- src/App.js
+++ src/App.js
@@ -1,31 +1,106 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import Header from './components/Header'
+import Wrapper from './components/Wrapper'
+import Card from "./components/Card"
+import images from "../public/images/"
 import Footer from './components/Footer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer></Footer>
-    </div>
+
+
+class App extends Component{
+  state ={
+    currentScore: 0,
+    topScore: 0,
+    cards, 
+    images
+  }
+
+
+gameOver = () => {
+  if (this.state.currentScore > this.state.topScore){
+    this.setState({topScore: this.state.currentScore}, function(){
+      console.log(this.state.topScore);
+      console.log(topScore);
+    })
+  }
+  this.state.cards.forEach(card => {
+    this.setState({currentScore:0})
+    card.count = 0;
+    return true;
+  });
+}
+  randomizeCards = () => {
+    this.state.images.sort(() => Math.random() - 0.5)
+    return true;
+  }
+
+  clickCard = id => {
+    this.state.cards.find((card,i) => {
+      if(card.id === id){
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1
+          this.setState({currentScore: this.state.score + 1}, function(){
+          console.log(this.state.currentScore)
+        })
+        this.randomizeCards();
+        return true
+      }
+      else{
+        this.gameOver();
+      }
+    }
+    })
+  }
+
+
+render() {
+  return(
+<div>
+  <Header currentScore={this.state.currentScore} topScore={this.state.score}>
+
+  </Header>
+  <Wrapper>
+   {this.state.cards.map (card => (
+    <Card
+      clickCard = {this.clickCard}
+      id = {card.id}
+      key = {card.id}
+      image={card.image}
+    />
+   ))}
+  </Wrapper>
+  <Footer>
+
+  </Footer>
+</div>
+  )
+}
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//       <Footer></Footer>
+//     </div>
 
     
-  );
-}
+//   );
+// }
 
 // render(){
 //   return(
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./style.css"
 
-const Header = props =>(
-	<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">Clicky Game</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+const Header = props =>{
+  return <div> <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">Clicky Game</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
@@ -17,13 +17,17 @@ const Header = props =>(
       </li>
     </ul>
   </div>
-</nav>
-<div className="jumbotron-backgroundImage">
-   <div className="transbox">
-   <h1>Mongo Scraper</h1>
-    <h3 className = "font-family: Cormorant SC">Wired Edition</h3>
-   </div>
+  </nav>
+  <div className="jumbotron-backgroundImage">
+  <div className="transbox">
+  <h1>Mongo Scraper</h1>
+   <h3 className = "font-family: Cormorant SC">Wired Edition</h3>
+  </div>
 </div>
-)
+	</div>
+}
+
 
 export default Header
+
+
